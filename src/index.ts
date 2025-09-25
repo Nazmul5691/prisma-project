1933360e5e6040fc86eab215b0ba309f4ea709e6
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-09-25 12:32:37 +0600

    finding-data-from-the-database

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,42 @@ const prisma = new PrismaClient()
 
 async function main() {
 
-    const result = await prisma.user.create({
-        data: {
-            name: 'sagor',
-            email: 'sagor@gmail.com',
-            // profilePhoto: 'http://nazmul/hero.jpg'
+    // const result = await prisma.user.create({
+    //     data: {
+    //         name: 'sagor',
+    //         email: 'sagor@gmail.com',
+    //         // profilePhoto: 'http://nazmul/hero.jpg'
            
+    //     }
+    // })
+
+    // console.log(result);
+
+
+    // const usersData = await prisma.user.findMany({
+    //     where: {
+    //         id: 2
+    //     }
+    // })
+
+    // console.log(usersData);
+
+    // const findUserById = await prisma.user.findUnique({
+    //     where: {
+    //         id: 3
+    //     }
+    // })
+
+    // console.log(findUserById);
+
+
+    const findUserById = await prisma.user.findUniqueOrThrow({
+        where: {
+            id: 7
         }
     })
 
-    console.log(result);
+    console.log(findUserById);
 }
 
 
